4140301c8d6fbcd137abf6b94f110898003300ef
Anders Sildnes <andsild@posteo.net>
2025-02-27 13:25:42 -0600

    compatible with docker-compose version less than 3

--- devops/dsa/launch.sh
+++ devops/dsa/launch.sh
@@ -1,1 +1,6 @@
-DSA_MONGODB_PORT=7987 DSA_PORT=7984 DSA_USER=1050:1050 docker compose -f docker-compose.yml -f docker-compose-override.yaml -p aza4423 up --build 
+#!/usr/bin/env bash
+
+build="${1:---build}"
+
+set -xe
+DSA_MONGODB_PORT=6061 DSA_PORT=6060 DSA_USER=$UID:$(id -g) docker compose -f docker-compose.yml -f docker-compose-override.yaml -p $USER up $build
